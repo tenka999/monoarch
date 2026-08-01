fd1be870dbf0206b2407ad1622580993e8fe03ce
tenka999 <keyzadepok95@gmail.com>
2026-08-02 00:51:54 +0700

    create testimonial section

--- frontend/src/presentation/views/home/HomePage.jsx
+++ frontend/src/presentation/views/home/HomePage.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./section/HeroSection";
 import ProductSection from "./section/ProductSection";
 import BenefitSection from "./section/BenefitSection";
 import ExploreSection from "./section/ExploreSection";
+import TestimonialSection from "./section/TestimonialSection";
 // import { ReactComponent as Logo } from "/layout/background/svg-path.svg";
 
 export default function HomePage() {
@@ -13,6 +14,7 @@ export default function HomePage() {
       <ProductSection />
       <BenefitSection />
       <ExploreSection />
+      <TestimonialSection />
     </div>
   );
 }
