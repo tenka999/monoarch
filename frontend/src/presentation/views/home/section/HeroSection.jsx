f06f31be05731a0cb2e37927f47133f4611045c6
tenka999 <keyzadepok95@gmail.com>
2026-08-03 11:24:13 +0700

    create hero section

---
+++ frontend/src/presentation/views/home/section/HeroSection.jsx
@@ -0,0 +1,15 @@
+import "@/styles/hero-style.css";
+
+export default function HeroSection() {
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h2>BEYOND COLOUR</h2>
+        <p>
+          Crafted in absolute black and white. Defining elegance without noise.
+        </p>
+      </div>
+      <div className="hero-button">EXPLORE COLLECTION</div>
+    </div>
+  );
+}
